43f6df2e8dcfe13b516279f6c58dbd917d8a200a
SRIMANI861 <narani.srimani@gmail.com>
2023-08-28 12:04:39 +0530

    comitting .js file

---
+++ JavaScript/Basic.js
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>JS Basic</title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            var name="Srimani";
+            var age=23;
+
+            var message="Hi, my name is"+name+" and I am "+age+"years old!";
+
+            alert(message);
+        </script>
+    </body>
+</html>
